db4d38788bdbb08b9cd9e7e0e96e52275b6f46ff
DevHoliday <devholidaydev@gmail.com>
2023-09-30 12:46:00 +0300

    versions of API

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,37 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express'
 import { RoutesInput, TErrorResponse, TProject } from '@/types/types'
-import structure from './structure.route'
 
 function isErrorProjects(data: TErrorResponse|{projects: TProject[]}): data is TErrorResponse {
     return !!(data as TErrorResponse).error;
 }
 
+const versions = ['2023-07'];
+
 export default ({ app }: RoutesInput) => {
+    app.use((req, res, next) => {
+        if (!req.accepts('application/json')) {
+            return res.status(406).json({ error: 'invalid_request'});
+        }
+        if (req.is('application/json') === false) {
+            return res.status(400).json({ error: 'invalid_request'});
+        }
+
+        next();
+    });
+
+    app.use((req, res, next) => {
+        const arUrl = req.originalUrl.split('/');
+        const versionFromUrl = arUrl[4];
+    
+        const isVersion = versions.indexOf(versionFromUrl);
+        const version = (isVersion === -1) ? versions[0]: versions[isVersion];
+    
+        res.header('X-Powered-By', 'AppFrame');
+        res.header('X-AppFrame-API-Version', version);
+
+        next();
+    });
+
     app.use(async function (req: Request, res: Response, next: NextFunction): Promise<void| Response> {
         try {
             const apiKey = req.headers['x-api-key'] as string;
@@ -48,5 +73,8 @@ export default ({ app }: RoutesInput) => {
         }
     });
 
-    app.use('/structures', structure);
+    for (const version of versions) {
+        const structures = require('./'+version+'/structure.route');
+        app.use(`/${version}/structures`, structures.default);        
+    }
 };
